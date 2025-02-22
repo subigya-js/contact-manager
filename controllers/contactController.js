6648c0df3e73d5e9e94cdd846f493af3b73eda78
subigya-js <developer.subigya@gmail.com>
2025-02-23 00:28:40 +0530

    feat: add express-async-handler for improved error handling in contact controller

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,21 +1,23 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc GET all contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc GET contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get contact for ${req.params.id}` });
-};
+});
 
 //@desc Create new contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   const { name, email, contact } = req.body;
 
   if (!name || !email || !contact) {
@@ -25,21 +27,21 @@ const createContact = (req, res) => {
 
   console.log("Request of POST", req.body);
   res.status(201).json({ message: "Contacts Created" }); // If we are creating something (POST Request), then we will give status as 201
-};
+});
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update contact for ${req.params.id}` });
-};
+});
 
 //@desc Delete contact
 //@route DELETE /api/contact/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contact for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
