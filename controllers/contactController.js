7b09cdce1bb3548fcff547055e92d904db24f2cb
subigya-js <developer.subigya@gmail.com>
2025-02-24 11:48:54 +0530

    feat: implement getContact function to retrieve contact by ID with error handling

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -13,7 +13,14 @@ const getContacts = asyncHandler(async (req, res) => {
 //@route GET /api/contacts/:id
 //@access public
 const getContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  res.status(200).json({ contact });
 });
 
 //@desc Create new contact
@@ -44,6 +51,7 @@ const createContact = asyncHandler(async (req, res) => {
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
+  
   res.status(200).json({ message: `Update contact for ${req.params.id}` });
 });
 
