dc2048a148df6d5155633e4fed988c8a1342434c
subigya-js <developer.subigya@gmail.com>
2025-02-25 09:33:46 +0530

    feat: enhance update and delete contact functions with error handling

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -51,15 +51,36 @@ const createContact = asyncHandler(async (req, res) => {
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
-  
-  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+  const contact = Contact.findById(req.params.id);
+
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json({ updatedContact });
 });
 
 //@desc Delete contact
 //@route DELETE /api/contact/:id
 //@access public
 const deleteContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+  const contact = Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  const deletedContact = await Contact.findByIdAndDelete(
+    req.params.id,
+    req.body
+  );
+  res.status(200).json({ deletedContact });
 });
 
 module.exports = {
