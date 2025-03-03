44cfce2764e38a549862ccdcf275e47b8fdd6bee
subigya-js <developer.subigya@gmail.com>
2025-03-03 09:50:55 +0530

    feat: add token validation middleware and update current user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -79,7 +79,7 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/current
 // @access private
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current User information." });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,12 @@ const {
   loginUser,
   currentUser,
 } = require("../controllers/userController");
+const validateToken = require("../middleware/validateTokenHandler");
 const router = express.Router();
 
 router
   .post("/register", registerUser)
   .post("/login", loginUser)
-  .get("/current", currentUser);
+  .get("/current", validateToken, currentUser);
 
 module.exports = router;
